d1b3e72945089ad5fb50e934bed3a0f74bf57edb
Subhash Barela <barelasubhash16@gmail.com>
2023-05-25 13:03:00 +0530

    [Subhash] Insert the customer details into table

--- CustomerDetails/CustomeData.sql
+++ CustomerDetails/CustomeData.sql
@@ -4,5 +4,12 @@ use CustomerDetails;
 --UC2_Create_Customer_Table
 create table Customer(CustomerId int primary key identity(1,1),Customer_Name varchar(30),PhoneNumber bigint not null,Address varchar(30));
 
-select * from Customer;
- CustomerDetails;
+
+--UC3_Insert_Values
+insert into Customer(Customer_Name,PhoneNumber,Address)
+values('Rahul',7658654675,'Latur'),
+	 ('Sagar',8978654601,'Pune'),
+	 ('Subhash',6788654644,'Jalgaon'),
+	 ('Mansii',9058654621,'Nashik')
+
+	 select * from Customer;
