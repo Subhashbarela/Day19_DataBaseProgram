866914243741e4ebbf41fc34836e721c64f9e198
Subhash Barela <barelasubhash16@gmail.com>
2023-05-25 12:55:57 +0530

    Create Database And Customer Table

--- CustomerDetails/CustomeData.sql
+++ CustomerDetails/CustomeData.sql
@@ -1,6 +1,8 @@
 --UC1_Create_Databe
 create database CustomerDetails;
+use CustomerDetails;
 --UC2_Create_Customer_Table
 create table Customer(CustomerId int primary key identity(1,1),Customer_Name varchar(30),PhoneNumber bigint not null,Address varchar(30));
 
 select * from Customer;
+ CustomerDetails;
