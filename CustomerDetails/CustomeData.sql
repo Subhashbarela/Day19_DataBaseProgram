1628d7feb9a03e9aff08e64e69087254821754f3
Subhash Barela <barelasubhash16@gmail.com>
2023-05-25 13:19:31 +0530

    [Subhash] Add new column into table

--- CustomerDetails/CustomeData.sql
+++ CustomerDetails/CustomeData.sql
@@ -1,15 +1,17 @@
---UC1_Create_Databe
+--UC1_Create_Databe and Create_Customer_Table
 create database CustomerDetails;
 use CustomerDetails;
---UC2_Create_Customer_Table
+
 create table Customer(CustomerId int primary key identity(1,1),Customer_Name varchar(30),PhoneNumber bigint not null,Address varchar(30));
 
 
---UC3_Insert_Values
+--UC2_Insert_Values
 insert into Customer(Customer_Name,PhoneNumber,Address)
 values('Rahul',7658654675,'Latur'),
 	 ('Sagar',8978654601,'Pune'),
 	 ('Subhash',6788654644,'Jalgaon'),
 	 ('Mansii',9058654621,'Nashik')
 
-	 select * from Customer;
+ select * from Customer;
+ --UC3_Add New Column in Existing table
+alter table Customer add  Country varchar(30),Salary varchar(30),Pincode int;
