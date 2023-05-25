4a55db71ca782605fad42638f003122d92b47152
Subhash Barela <barelasubhash16@gmail.com>
2023-05-25 12:51:03 +0530

    [Subhash] Create Database Project

---
+++ CustomerDetails/CustomeData.sql
@@ -0,0 +1,6 @@
+--UC1_Create_Databe
+create database CustomerDetails;
+--UC2_Create_Customer_Table
+create table Customer(CustomerId int primary key identity(1,1),Customer_Name varchar(30),PhoneNumber bigint not null,Address varchar(30));
+
+select * from Customer;
