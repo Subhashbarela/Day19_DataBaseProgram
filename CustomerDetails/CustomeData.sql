ebcde107ffdaab809033ec5fa012885d6d034c37
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-05-25 15:41:11 +0530

    Update CustomeData.sql

--- CustomerDetails/CustomeData.sql
+++ CustomerDetails/CustomeData.sql
@@ -20,11 +20,10 @@ alter table Customer set Country ='India'  where id= 1;
 --UC4_Update table
 update Customer set Address='Jalgaon' where Customer_Name='Avinash';
 update Customer set Address='Nagpur' where Customer_Name='Vikas';
-<<<<<<< HEAD
-=======
+
 
 --UC5_Number of customer in each country
 select count(Country)as 'Total Customer',Country from Customer group by(Country);
 
 
->>>>>>> UC5_FindCustomerInCountryWise
+
